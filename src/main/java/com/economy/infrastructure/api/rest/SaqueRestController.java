3b4bd29beb794c661ae88965d8dbc35adeeec546
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-18 23:12:12 -0300

    classes configs feitas

--- src/main/java/com/economy/infrastructure/api/rest/SaqueRestController.java
+++ src/main/java/com/economy/infrastructure/api/rest/SaqueRestController.java
@@ -2,6 +2,8 @@ package com.economy.infrastructure.api.rest;
 
 import com.economy.domain.model.Saque;
 import com.economy.domain.service.SaqueService;
+import com.economy.interfaces.SaqueController;
+import com.economy.interfaces.SaqueControllerImpl;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -12,23 +14,23 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class SaqueRestController {
-    private final SaqueService saqueService;
+    private final SaqueController saqueController;
 
-    public SaqueRestController(SaqueService saqueService) {
-        this.saqueService = saqueService;
+    public SaqueRestController(SaqueController saqueController) {
+        this.saqueController = saqueController;
     }
 
     @POST
     @Path("/pix")
     public Response emitirSaquePix(Saque saque) {
-        Saque novoSaque = saqueService.criarSaque(saque);
+        Saque novoSaque = saqueController.criarSaque(saque);
         return Response.ok(novoSaque).build();
     }
 
     @GET
     @Path("/empregado/{empregadoId}")
     public Response listarSaquesPorEmpregado(@PathParam("empregadoId") int empregadoId) {
-        List<Saque> saques = saqueService.listarByEmpregadoId(empregadoId);
+        List<Saque> saques = saqueController.listarByEmpregadoId(empregadoId);
         return Response.ok(saques).build();
     }
 }
